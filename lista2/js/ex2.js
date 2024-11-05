90fb3296df0b7bac8bde619413cc853bab3eeabf
gui07777 <guilherme2006.gmc@gmail.com>
2024-11-05 20:28:58 -0300

    Add files via upload

---
+++ lista2/js/ex2.js
@@ -0,0 +1,7 @@
+var valor = parseFloat(prompt("Digite um valor:"));
+
+if(valor>=0){
+alert("O valor é positivo");
+}else{
+    alert("O valor é negativo");
+}
---
+++ lista2/js/ex3.js
@@ -0,0 +1,11 @@
+var letra = prompt("Digite uma letra:");
+
+if( letra == "M" ){
+alert("M-MASCULINO");}
+    
+if(letra == "F"){
+alert("F-FEMININO");
+
+}else{
+    alert("Inválido");}
+
