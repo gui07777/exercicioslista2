07bcf00b980c0b99e6208f1408994a6aa16cfb2e
gui07777 <guilherme2006.gmc@gmail.com>
2024-11-16 13:06:19 -0300

    Delete lista2 directory

--- lista2/js/ex17.js
+++
@@ -1,7 +0,0 @@
-var ano = parseInt(prompt("Digite um ano para verificar se é bissexto:"));
-
-if ((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)) {
-  console.log(ano + " é um ano bissexto.");
-} else {
-  console.log(ano + " não é um ano bissexto.");
-}
--- lista2/js/ex2.js
+++
@@ -1,7 +0,0 @@
-var valor = parseFloat(prompt("Digite um valor:"));
-
-if(valor>=0){
-alert("O valor é positivo");
-}else{
-    alert("O valor é negativo");
-}
