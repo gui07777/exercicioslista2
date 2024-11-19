3346ce4140e334ffc8125e02b15c3a9862b09368
gui07777 <guilherme2006.gmc@gmail.com>
2024-11-19 19:59:17 -0300

    Add files via upload

---
+++ Exercicioslogica2/js/ex3.js
@@ -0,0 +1,10 @@
+var letra = prompt("Letra do seu sexo:");
+
+if(letra === "F" || letra === "f"){
+    alert("F-FEMININO")
+}
+else if ( letra === "M" || letra === "m"){
+    alert("M-MASCULINO")
+} else {
+    alert("Inválido")
+}
