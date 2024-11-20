9cf35640fcf41ca2188e6866801df332385e0e38
gui07777 <guilherme2006.gmc@gmail.com>
2024-11-19 21:32:33 -0300

    Delete Exercicioslogica2/ex1.js

--- Exercicioslogica2/ex1.js
+++
@@ -1,8 +0,0 @@
-var n1 = parseFloat(prompt("Digite o primeiro número:"));
-var n2 = parseFloat(prompt("Digite o segundo número:"));
-
-if(n1 > n2){
-    alert("O maior é:" + n1)
-}else{
-    alert("O maior é:" + n2)
-}
